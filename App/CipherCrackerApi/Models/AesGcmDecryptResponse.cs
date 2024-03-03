a065545601d329a6005655406ed29a86b1bd96d8
st_Akey <qwer0516yp@gmail.com>
2024-03-03 20:48:37 +1100

    keep this git repo classLibrary & its related uts only

--- App/CipherCrackerApi/Models/AesGcmDecryptResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using Newtonsoft.Json;
-
-namespace CipherCracker.Api.Models;
-
-public class AesGcmDecryptResponse : ResponseBase
-{
-    [JsonProperty("plainText")]
-    public string PlainText { get; set; }
-}
