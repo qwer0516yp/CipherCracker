1e700610ae8332481c75ef997be16ded8af25c40
st_Akey <qwer0516yp@gmail.com>
2024-04-17 21:32:55 +1000

    workaround bc ToRSA issue on Linux. https://github.com/bcgit/bc-csharp/issues/160

--- Src/CipherCracker.ClassLibrary/Extensions/RsaKeyExtensions.cs
+++ Src/CipherCracker.ClassLibrary/Extensions/RsaKeyExtensions.cs
@@ -2,5 +2,10 @@
 
 public static class RsaKeyExtensions
 {
-    public static RSA ToRSA(this AsymmetricKeyParameter privateKeyParameter) => DotNetUtilities.ToRSA((RsaPrivateCrtKeyParameters)privateKeyParameter);
+    public static RSA ToRSA(this AsymmetricKeyParameter privateKeyParameter)
+    {
+        var rsa = RSA.Create();
+        rsa.ImportParameters(DotNetUtilities.ToRSAParameters((RsaPrivateCrtKeyParameters)privateKeyParameter));
+        return rsa;
+    }
 }
